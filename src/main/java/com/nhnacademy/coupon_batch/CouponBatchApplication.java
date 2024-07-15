588d0d9a999a45fb8e4f995b18bd247951b84585
jjeonmin <jms959310@gmail.com>
2024-07-15 20:36:41 +0900

    feat:init

---
+++ src/main/java/com/nhnacademy/coupon_batch/CouponBatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.coupon_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponBatchApplication.class, args);
+    }
+
+}
